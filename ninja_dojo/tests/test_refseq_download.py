71298f1bfecd0c49c08f5161bb3bf4fbbfb8f3a4
Benjamin Hillmann <bhillmann@outlook.com>
2016-06-07 10:13:18 -0500

    Updated the testing name

--- ninja_dojo/tests/test_refseq_download.py
+++ ninja_dojo/tests/test_refseq_download.py
@@ -3,7 +3,7 @@ from nose.tools import assert_equals
 from ninja_dojo.downloaders import RefseqSummary
 
 
-def test_homo_sapiens_gg_lineage_ncbi_tree():
+def test_refseq_download():
     downloader = RefseqSummary()
     downloader.run()
     assert_equals(None, None)
