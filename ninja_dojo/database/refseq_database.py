59701494de8f8cc0cdcbf6e0da31a1bc81d85925
Benjamin Hillmann <bhillmann@outlook.com>
2016-06-21 16:32:28 -0500

    Starting the SQLITE database

--- ninja_dojo/database/refseq_database.py
+++ ninja_dojo/database/refseq_database.py
@@ -1,5 +1,6 @@
 from ninja_trebuchet.factory import download
 import sqlite3
+import os
 
 from .. import SETTINGS, LOGGER
 from ..downloaders import RefseqCatalog, RefseqAssemblySummary, NCBITaxdmp
@@ -11,4 +12,8 @@ class RefSeqDatabase:
 
     @download
     def _parse(self):
-        SETTINGS.
+        db_path = os.path.join(SETTINGS.get_path('db_dir'), 'refseq.db')
+        conn = sqlite3.connect(db_path)
+        c = conn.cursor()
+
+        # c.execute('''CREATE TABLE tree (date )''')
