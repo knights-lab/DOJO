a4660ad956540a6e3a83661c433812f8a1e3d110
Benjamin Hillmann <bhillmann@outlook.com>
2016-07-20 15:08:28 -0500

    Added the new test for ncbi tree.

--- ninja_dojo/tests/test_ncbi_tree.py
+++ ninja_dojo/tests/test_ncbi_tree.py
@@ -1,8 +1,11 @@
+import unittest
 from nose.tools import assert_equals
 
 from ninja_dojo.taxonomy import NCBITree
 
 
-def test_homo_sapiens_gg_lineage_ncbi_tree():
-    ncbi_tree = NCBITree()
-    assert_equals(ncbi_tree.gg_lineage(9606), 'k__Eukaryota;p__Chordata;c__Mammalia;o__Primates;f__Hominidae;g__Homo;s__Homo_sapiens')
+class TestGGLineage(unittest.TestCase):
+    def test(self):
+        ncbi_tree = NCBITree()
+        assert_equals(ncbi_tree.gg_lineage(9606),
+                      'k__Eukaryota;p__Chordata;c__Mammalia;o__Primates;f__Hominidae;g__Homo;s__Homo_sapiens')
