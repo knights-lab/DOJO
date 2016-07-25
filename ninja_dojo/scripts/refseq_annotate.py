e9e16a1abef39f269746c68bb932754581e0b602
Benjamin Hillmann <bhillmann@outlook.com>
2016-07-25 13:09:12 -0500

    Added the annotate refix selector.

--- ninja_dojo/scripts/refseq_annotate.py
+++ ninja_dojo/scripts/refseq_annotate.py
@@ -10,13 +10,17 @@ from ninja_dojo.database import RefSeqDatabase
 @click.command()
 @click.option('-i', '--input', type=click.File('r'))
 @click.option('-o', '--output', type=click.File('w'), default='-')
+@click.option('-x', '--extract_refseq_id', default='ref|,|')
 @click.option('--prefixes', default='*', help="Supply a comma-seperated list where the options are choices"
                                               " in ('AC', 'NC', 'NG', 'NM', 'NT', 'NW', 'NZ') e.g. NC,AC default=all")
-def refseq_annotate(input, output, prefixes):
+def refseq_annotate(input, output, extract_refseq_id, prefixes):
     db = RefSeqDatabase()
 
     # check for the glob prefix
     prefixes = prefixes.split(',')
+
+    begin, end = extract_refseq_id.split(',')
+
     if '*' in prefixes:
         prefix_set = set([_ for _ in db.refseq_prefix_mapper.keys()])
     else:
@@ -24,7 +28,7 @@ def refseq_annotate(input, output, prefixes):
 
         inf_fasta = FASTA(input)
         for title, seq in inf_fasta.read():
-            refseq_accession_version = find_between(title, 'ref|', '|')
+            refseq_accession_version = find_between(title, begin, end)
             if refseq_accession_version[:2] in prefix_set:
                 ncbi_tid = db.get_ncbi_tid_from_refseq_accession_version(refseq_accession_version)
                 if ncbi_tid:
