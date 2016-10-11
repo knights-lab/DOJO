96dc661987c6dfa6798f33ce40e8f4a6e983d9f1
Benjamin Hillmann <bhillmann@outlook.com>
2016-10-11 14:19:53 -0500

    Added the nucleotide.

--- ninja_dojo/annotaters/__init__.py
+++ ninja_dojo/annotaters/__init__.py
@@ -1,5 +1,5 @@
 from .gi import GIAnnotater
 from .refseq import RefSeqAnnotater
-from .nt import NTAnnotater
+from .nucleotide import NTAnnotater
 
 __all__ = ['GIAnnotater', 'RefSeqAnnotater', 'NTAnnotater']
