6e4fb049b879ea7ce2f56a318aaea2f69b0c6646
Benjamin Hillmann <bhillmann@outlook.com>
2017-01-23 10:54:34 -0600

    Added the new NCBI annotater.

---
+++ dojo/annotaters/ncbi.py
@@ -0,0 +1,25 @@
+from ninja_utils.utils import find_between
+
+from .annotater import Annotater
+
+
+class NCBIAnnotater(Annotater):
+    def __init__(self, extraction_ncbi_tid, tree, depth=7, depth_force=True):
+        Annotater.__init__(self)
+
+        self.begin, self.end = extraction_ncbi_tid.split(',')
+
+        self.tree = tree
+        self.depth = depth
+        self.depth_force = depth_force
+
+    def annotate(self, gen_fasta):
+        for title, seq in gen_fasta:
+            title = '>' + title
+            ncbi_tid = find_between(title, self.begin, self.end)
+            if ncbi_tid:
+                gg = self.tree.green_genes_lineage(ncbi_tid[0], depth=self.depth, depth_force=self.depth_force)
+                if gg:
+                    gg = '; '.join(gg.split(';'))
+                    header = 'ncbi_tid|%d|%s' % (ncbi_tid[0], title[1:])
+                    yield '>%s\n%s\n' % (header, seq), '%s\t%s\n' % (header.split()[0], gg)
